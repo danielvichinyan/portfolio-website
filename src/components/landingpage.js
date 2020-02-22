c9efb27b8d1117da07147cffd2f637275439e855
danielvichinyan <45153745+danielvichinyan@users.noreply.github.com>
2020-02-23 00:02:06 +0200

    Added css styling for the home page. Added styled social links and image.

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,11 +1,50 @@
 import React, { Component } from 'react';
-
+import { Grid, Cell } from 'react-mdl';
+import profilePic from '../assets/images/prof-pic.jpg';
 class Landing extends Component {
 
     render() {
         return(
-            <div>
-            <h1>Landing Page</h1>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                        <img 
+                            src={profilePic}
+                            alt="profilePicture"
+                            className="profile-pic"
+                        />
+
+                        <div className="banner-text">
+                            <h1>Software Developer</h1>
+
+                            <hr />
+
+                            <p>Java | Spring | Maven | AWS | Swing | jUnit | Oracle | HTML/CSS/PHP | Ruby on Rails | Bootstrap | JavaScript | React.js | REST | jQuery | Git | GitHub | Linux</p>
+
+                            <div className="social-links">
+                                {/* LinkedIn */}
+                                <a href="https://www.linkedin.com/in/danielvichinyan/" target="_blank" rel="noopener noreferrer">
+                                    <i className="fa fa-linkedin-square" aria-hidden="true" />
+                                </a>
+
+                                {/* GitHub */}
+                                <a href="https://github.com/danielvichinyan" target="_blank" rel="noopener noreferrer">
+                                    <i className="fa fa-github-square" aria-hidden="true" />
+                                </a>
+
+                                {/* FreeCodeCamp */}
+                                <a href="https://www.hackerrank.com/vichinyan_daniel" target="_blank" rel="noopener noreferrer">
+                                    <i className="fa fa-free-code-camp" aria-hidden="true" />
+                                </a>
+
+                                {/* YouTube */}
+                                <a href="https://www.hackerrank.com/vichinyan_daniel" target="_blank" rel="noopener noreferrer">
+                                    <i className="fa fa-youtube-square" aria-hidden="true" />
+                                </a>
+                            </div>
+                        </div>
+                    </Cell>
+                </Grid>
             </div>
         )
     }
